a9c8556583d126b64ccddd897c815940583387c3
Simmons, John <JohnSimmons@quickenloans.com>
2016-10-17 15:16:56 -0400

    username to view

--- app/auth/views.py
+++ app/auth/views.py
@@ -32,7 +32,7 @@ def register():
 	form = RegistrationForm()
 	if form.validate_on_submit():
 	    user = User(email=form.email.data,
-	                password=form.password.data)
+	                password=form.password.data, username=form.username.data)
 	    db.session.add(user)
 	    db.session.commit()
 	    token = user.generate_confirmation_token()
