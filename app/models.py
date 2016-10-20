73071c67157884a1d90921f1cdf166cf003a5ad8
Simmons, John <JohnSimmons@quickenloans.com>
2016-10-20 14:26:31 -0400

    ch user

--- app/models.py
+++ app/models.py
@@ -19,7 +19,7 @@ class Role(db.Model):
 	permissions = db.Column(db.Integer)
 	
 	# FK & Relationship
-	users = db.relationship('User', backref='role', lazy='dynamic')
+	users_fk = db.relationship('User', backref='role', lazy='dynamic')
 
 
 	@staticmethod
