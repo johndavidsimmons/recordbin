b146e8577ccf5f226980b60b82fad733b3e2f4a1
angieellis <angiegrace84@gmail.com>
2017-10-22 16:34:10 +0700

    add factories for easier testing

---
+++ tests/factories/__init__.py
@@ -0,0 +1,4 @@
+''' Factories can make things much easier and cleaner when it comes
+to testing, especially as your test suite starts getting larger.
+Here I am using Factory Boy, a module that allows easy creation of factories.
+'''
---
+++ tests/factories/user.py
@@ -0,0 +1,19 @@
+import factory
+
+from app import db
+from app.models import Role, User
+
+
+class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
+
+    class Meta:
+        model = User
+        sqlalchemy_session = db.session
+        sqlalchemy_session_persistence = 'commit'
+
+    email = factory.Sequence(lambda n: 'user{}@example.com'.format(n))
+    username = factory.Sequence(lambda n: 'user{}'.format(n))
+    password = 'abc123'
+    about_me = 'hello'
+    location = 'world'
+    confirmed = True
